21e4be5f35a57daa8d56a6ca4736ea1d800648d5
Jagadish Kasi <jagadish.kasi@walmart.com>
2018-09-12 13:30:17 +0530

    removed redundant code.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 // Transpile all code following this line with babel and use 'env' (aka ES6) preset.
 
 require('dotenv').config()
-if(process.env.NODE_ENV !== 'production'){
-  require('@babel/register')({
-    presets: [ '@babel/preset-env', ]
-  });
-} else {
-  process.env.PORT = 80;
-}
 
 module.exports = require('./app.js')
